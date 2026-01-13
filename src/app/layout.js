70b4563bae9eb9ff6e0adcbf4dc150a4cb63fb00
AbdulRehmanAarmaan <abdulrehmanaarmaan@gmail.com>
2026-01-13 12:48:59 +0600

    initialize project with Next.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +25,12 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Toaster></Toaster>
+        <Navbar></Navbar>
+
+        <main className='pt-6 pb-16'>{children}</main>
+
+        <Footer></Footer>
       </body>
     </html>
   );
